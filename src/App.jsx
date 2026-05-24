92124bae7c35c147791ebc8aafb3870724863d4e
paullouismarfil <iyong-email@gmail.com>
2026-05-24 10:58:49 +0800

    fix global ui size

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import CollectionStaffPortal from "./pages/CollectionStaffPortal";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/lgu" element={<LGUPortal />} />
-      <Route path="/barangay" element={<BarangayPortal />} />
-      <Route path="/collector" element={<CollectionStaffPortal />} />
-    </Routes>
+    <div className="min-h-screen w-full overflow-x-hidden bg-slate-50 text-slate-900">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/lgu" element={<LGUPortal />} />
+        <Route path="/barangay" element={<BarangayPortal />} />
+        <Route path="/collector" element={<CollectionStaffPortal />} />
+      </Routes>
+    </div>
   );
 }
