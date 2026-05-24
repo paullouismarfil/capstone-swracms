16a1135ab0d8847f9f10f234605bf129ab8dab8d
paullouismarfil <iyong-email@gmail.com>
2026-05-24 11:01:28 +0800

    revert global layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,11 @@ import CollectionStaffPortal from "./pages/CollectionStaffPortal";
 
 export default function App() {
   return (
-    <div className="min-h-screen w-full overflow-x-hidden bg-slate-50 text-slate-900">
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/lgu" element={<LGUPortal />} />
-        <Route path="/barangay" element={<BarangayPortal />} />
-        <Route path="/collector" element={<CollectionStaffPortal />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/lgu" element={<LGUPortal />} />
+      <Route path="/barangay" element={<BarangayPortal />} />
+      <Route path="/collector" element={<CollectionStaffPortal />} />
+    </Routes>
   );
 }
